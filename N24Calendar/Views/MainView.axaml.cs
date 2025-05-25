9446f435dac470a537145c3eeeb0a53b3b7a9849
Aberro <aberroarman@gmail.com>
2025-05-25 17:00:58 +0200

    I have no idea what I'm doing.

---
+++ N24Calendar/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace N24Calendar.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ N24Calendar/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace N24Calendar.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
